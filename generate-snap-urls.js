dc61a6a50001ce14ea69fbe3eee41726baf6c2d6
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2026-04-25 18:07:52 +0700

    Remove react snap since its outdated

--- generate-snap-urls.js
+++ generate-snap-urls.js
@@ -1,21 +1,7 @@
 import fs from 'fs'
+
 const gems = JSON.parse(fs.readFileSync('./src/data/gems.json', 'utf8'))
 
-const urls = [
-  '/',
-  '/about',
-  '/contact',
-  '/wishlist',
-  '/compare',
-  ...gems.map(g => `/gem/${g.id}`)
-]
-
-// Update package.json reactSnap urls
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
-pkg.reactSnap.include = urls
-fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
-
-// Generate sitemap.xml
 const baseUrl = 'https://cgt.onl'
 
 const staticUrls = [
@@ -43,5 +29,4 @@ ${allUrls.map(u => `  <url>
 
 fs.writeFileSync('./public/sitemap.xml', sitemap)
 
-console.log(`✅ Generated ${urls.length} URLs for react-snap`)
 console.log(`✅ Generated sitemap.xml with ${allUrls.length} entries`)
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import { hydrateRoot, createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
 
-const rootElement = document.getElementById('root')
-
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </React.StrictMode>
-  )
-} else {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </React.StrictMode>
-  )
-}
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+)
